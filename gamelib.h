5ff53518da3f5de0dd581a9ed91914b734720550
alexdru11 <94228277+alexdru11@users.noreply.github.com>
2024-12-10 18:39:00 +0100

    Update gamelib.h

--- gamelib.h
+++ gamelib.h
@@ -5,87 +5,76 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Numero massimo di giocatori
+// Dimensioni massime per i nomi
+#define MAX_NAME_LENGTH 50
+#define MAX_ROOMS 15
 #define MAX_PLAYERS 3
 
-// Lunghezza massima del nome del giocatore
-#define MAX_NAME_LENGTH 50
-
-// Numero di stanze generate casualmente
-#define NUM_RANDOM_ROOMS 15
-
-// Enumerazione per il tipo di giocatore
+// Enumerazione per la classe del giocatore
 typedef enum {
-    PRINCIPE,
-    DOPPLEGANGER
+    principe,
+    doppleganger
 } tipo_giocatore;
 
 // Enumerazione per il tipo di stanza
 typedef enum {
-    CORRIDOIO,
-    SCALA,
-    SALA_BANCHETTO,
-    MAGAZZINO,
-    POSTO_GUARDIA,
-    PRIGIONE,
-    ARMERIA,
-    MOSCHEA,
-    TORRE,
-    BAGNI
+    corridoio,
+    scala,
+    sala_banchetto,
+    magazzino,
+    posto_guardia,
+    prigione,
+    armeria,
+    moschea,
+    torre,
+    bagni
 } Tipo_stanza;
 
 // Enumerazione per il tipo di trabocchetto
 typedef enum {
-    NESSUNO,
-    TEGOLA,
-    LAME,
-    CADUTA,
-    BURRONE
+    nessuno,
+    tegola,
+    lame,
+    caduta,
+    burrone
 } Tipo_trabocchetto;
 
 // Enumerazione per il tipo di tesoro
 typedef enum {
-    NESSUN_TESORO,
-    VERDE_VELENO,
-    BLU_GUARIGIONE,
-    ROSSO_AUMENTA_VITA,
-    SPADA_TAGLIENTE,
-    SCUDO
+    nessun_tesoro,
+    verde_veleno,
+    blu_guarigione,
+    rosso_aumenta_vita,
+    spada_tagliente,
+    scudo
 } Tipo_tesoro;
 
-// Struttura per rappresentare una stanza
-typedef struct Stanza {
-    Tipo_stanza tipo;
-    Tipo_trabocchetto trabocchetto;
-    Tipo_tesoro tesoro;
-    struct Stanza* stanza_destra;
-    struct Stanza* stanza_sinistra;
-    struct Stanza* stanza_sopra;
-    struct Stanza* stanza_sotto;
-} Stanza;
-
-// Struttura per rappresentare un giocatore
-typedef struct Giocatore {
+// Struttura per il giocatore
+typedef struct {
     char nome_giocatore[MAX_NAME_LENGTH];
-    tipo_giocatore tipo;
-    Stanza* posizione;
+    tipo_giocatore classe;
+    struct Stanza *posizione;
     unsigned char p_vita_max;
     unsigned char p_vita;
     unsigned char dadi_attacco;
     unsigned char dadi_difesa;
-    int turni_saltati;
 } Giocatore;
 
-// Funzioni principali (dichiarazioni)
+// Struttura per la stanza
+typedef struct Stanza {
+    Tipo_stanza tipo;
+    Tipo_trabocchetto trabocchetto;
+    Tipo_tesoro tesoro;
+    struct Stanza *stanza_destra;
+    struct Stanza *stanza_sinistra;
+    struct Stanza *stanza_sopra;
+    struct Stanza *stanza_sotto;
+} Stanza;
+
+// Funzioni di gamelib.c
 void imposta_gioco();
 void gioca();
 void termina_gioco();
 void crediti();
 
-// variabili globali
-int mappa_creata;
-Stanza *pFirst;
-Giocatore* giocatori[MAX_PLAYERS];
-char nomi_vincitori[3][MAX_NAME_LENGTH];
-int indice_vincitori;
 #endif
