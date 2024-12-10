450222600259927b37de2262b8892e1b28fecade
alexdru11 <94228277+alexdru11@users.noreply.github.com>
2024-12-10 18:42:02 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,36 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "gamelib.h"
 
-int main()
-{
+int main() {
+    // Inizializza il generatore di numeri casuali
+    srand((unsigned)time(NULL));
+
     int scelta;
 
-    do
-    {
+    do {
         printf("\nMenu Principale:\n");
-        printf("1. Imposta gioco\n");
-        printf("2. Gioca\n");
-        printf("3. Termina gioco\n");
-        printf("4. Visualizza crediti\n");
-        printf("Scegli un'opzione: ");
+        printf("1 - Imposta gioco\n");
+        printf("2 - Gioca\n");
+        printf("3 - Termina gioco\n");
+        printf("4 - Crediti\n");
+        printf("Scelta: ");
         scanf("%d", &scelta);
 
-        switch (scelta)
-        {
-        case 1:
-            imposta_gioco();
-            break;
-        case 2:
-            gioca();
-            break;
-        case 3:
-            termina_gioco();
-            break;
-        case 4:
-            crediti();
-            break;
-        default:
-            printf("Comando non valido.\n");
+        switch (scelta) {
+            case 1:
+                imposta_gioco();
+                break;
+            case 2:
+                gioca();
+                break;
+            case 3:
+                termina_gioco();
+                break;
+            case 4:
+                crediti();
+                break;
+            default:
+                printf("Comando non valido.\n");
         }
     } while (scelta != 3);
 
