870f7ef998d92128d63aec3b03aeae858c9176c6
alexdru11 <94228277+alexdru11@users.noreply.github.com>
2025-01-01 23:39:51 +0100

    Update gamelib.h

--- gamelib.h
+++ gamelib.h
@@ -1,14 +1,9 @@
 #ifndef GAMELIB_H
 #define GAMELIB_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
-
-#define MAX_NAME_LENGTH 50
-#define MAX_ROOMS 15
 #define MAX_PLAYERS 3
+#define MAX_ROOMS 15
+#define MAX_NAME_LENGTH 20
 
 // Enumerazione per la classe del giocatore
 typedef enum {
@@ -53,12 +48,12 @@ typedef enum {
 typedef struct {
     char nome_giocatore[MAX_NAME_LENGTH];
     Tipo_giocatore classe;
-    struct Stanza *posizione;
     unsigned char p_vita_max;
     unsigned char p_vita;
     unsigned char dadi_attacco;
     unsigned char dadi_difesa;
-    int fuggito; // Contatore per le fughe, 0 = non è fuggito, 1 = 1 volta, 2 = 2 volte (per i doppleganger)
+    struct Stanza *posizione;
+    int fuggito;
 } Giocatore;
 
 // Struttura per la stanza
@@ -66,15 +61,12 @@ typedef struct Stanza {
     Tipo_stanza tipo;
     Tipo_trabocchetto trabocchetto;
     Tipo_tesoro tesoro;
-    int trabocchetto_attivato; // Flag per indicare se il trabocchetto è già stato attivato da un giocatore
-    int tentativi_porta[4];  // Contatore tentativi per ogni direzione (0: Sopra, 1: Sotto, 2: Sinistra, 3: Destra)
-    struct Stanza *stanza_sopra;
-    struct Stanza *stanza_sotto;
-    struct Stanza *stanza_sinistra;
-    struct Stanza *stanza_destra;
+    struct Stanza *stanza_successiva;
+    int trabocchetto_attivato;
+    int tentativi_porta[4];
 } Stanza;
 
-// Funzioni di gamelib.c
+// Funzioni
 void imposta_gioco();
 void gioca();
 void termina_gioco();
